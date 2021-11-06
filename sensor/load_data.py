6a527550a248022affa65f08999e05e4d30bc33d
pksnj <sharmapraveenk@gmail.com>
2021-11-06 14:47:58 -0400

    loaddata

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -1,14 +1,15 @@
- import os as os
- import glob as glob
- import csv as csv
+import os as os
+import glob as glob
+import csv as csv
 
- def load_sensor_data():
+
+def load_sensor_data():
     sensor_data = []
     sensor_files = glob.glob(os.path.join(os.getcwd(), 'datasets', '*.csv'))
-    for sensor_file in sensor_files
+    for sensor_file in sensor_files:
         with open(sensor_file, 'r') as data_file:
             data_reader = csv.DictReader(data_file, delimiter=',')
-            for row in data_reader
+            for row in data_reader:
                 sensor_data = sensor_data.append(row)
 
     return(sensor_data)
